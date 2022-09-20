6fca1d73cb03794f30e2b7f7494182fb1b23b671
Tysane Norine <tysaneno@ev-h2.kzn.21-school.ru>
2022-09-20 20:38:36 +0300

    part_1

--- src/01/main.sh
+++ src/01/main.sh
@@ -2,9 +2,14 @@
 
 re='^[0-9]+$'
 
-if [[ $1 =~ $re ]]
-then 
-echo "Error, only string"
-else 
-echo $1
+if [[ $# == 1 ]] 
+  then
+  if [[ $1 =~ $re ]]
+    then 
+    echo "Error, only string"
+  else 
+  echo $1
+  fi
+else
+echo "Error, only one parameter"
 fi
