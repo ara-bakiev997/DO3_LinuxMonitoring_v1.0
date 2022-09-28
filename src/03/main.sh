2aff2d9da46dcf14fe672f705365a1b75f94b6cf
ara-bakiev997 <bakiev_bai@mail.ru>
2022-09-28 20:22:27 +0300

    part_3 full+check_error

--- src/03/main.sh
+++ src/03/main.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
+argv[0]=$1
+argv[1]=$2
+argv[2]=$3
+argv[3]=$4
+
+for (( i = 0; i < $#; i++))
+do
+    if [[ ${argv[$i]} -le 0 || ${argv[$i]} -ge 7 ]]
+        then
+        echo -e "1 - white, 2 - red, 3 - green, 4 - blue, 5 – purple, 6 - black\nRun the script again"
+        exit 1
+    fi
+done
+
 if [[ $# == 4 ]] 
     then 
     if [[ $1 -eq $2 || $3 -eq $4 ]]
         then
-        echo "Error! Parameter_1 != Parameter_2 and Parameter_3 != Parameter_4. 
-       1 - white, 2 - red, 3 - green, 4 - blue, 5 – purple, 6 - black
-       Run the script again"
+        echo -e "Error! Parameter_1 != Parameter_2 and Parameter_3 != Parameter_4\nRun the script again"
+       exit 1
         else
         chmod +x inst_supp.sh
         bash inst_supp.sh
